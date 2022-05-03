870d452b521ab5f111c50b9e6dbced20f43cbdb9
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2022-05-03 09:59:10 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui',],
+      'serif': ['ui-serif', 'Georgia',],
+      'mono': ['consolas'],
+      'display': ['Oswald',],
+      'body': ['"Open Sans"'],
+    },
+  },
+  plugins: [],
+  darkMode: 'class',
+}
